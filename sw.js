6abaa67e9711aaf9265d230e724e53566d945726
T-Makishi <makishi0520@gmail.com>
2026-07-23 07:41:18 +0900

    Service Worker更新を安定化

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE='pcsapo-sales-v18';
+const CACHE='pcsapo-sales-v19';
 const ASSETS=['./','./index.html','./assets/css/style.css','./assets/js/app.js','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png','./assets/docs/square-funfo-presentation.pdf'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([
   caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),
   self.clients.claim()
-]).then(()=>self.clients.matchAll({type:'window'})).then(clients=>Promise.all(clients.map(client=>client.navigate(client.url))))));
+])));
 self.addEventListener('fetch',event=>{
   if(event.request.mode==='navigate'){
     event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));
