ddb6d0851810cad9a1f1754800222aaa705c8e79
T-Makishi <makishi0520@gmail.com>
2026-07-23 11:09:37 +0900

    funfoオンライン決済の審査案内を追加

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='pcsapo-sales-v36';
+const CACHE='pcsapo-sales-v37';
 const ASSETS=['./','./index.html','./assets/css/style.css','./assets/js/app.js','./manifest.webmanifest','./assets/icons/favicon-32.png','./assets/icons/icon-192.png','./assets/icons/icon-512.png','./assets/images/pcsapo-logo.png','./assets/images/pcsapo-stamp.png','./assets/docs/square-funfo-presentation.pdf'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(Promise.all([
